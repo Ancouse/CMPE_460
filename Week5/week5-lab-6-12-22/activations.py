4449c17579b9f15a6702f1f5ea587e4f4eb8ec0f
Ancouse <89256468+Ancouse@users.noreply.github.com>
2022-12-07 05:11:14 +0300

    7/12/22

--- Week5/week5-lab-6-12-22/activations.py
+++ Week5/week5-lab-6-12-22/activations.py
@@ -16,7 +16,7 @@ class Activation:
     def _derivate(x: np.ndarray):
         raise NotImplementedError
 
-
+#it looks good
 class Tanh(Activation):
     @staticmethod
     def _normal(x: np.ndarray):
@@ -26,38 +26,38 @@ class Tanh(Activation):
     def _derivate(x: np.ndarray):
         return 1 - np.tanh(x) ** 2
 
-
+# it looks good.
 class Sigmoid(Activation):
     @staticmethod
     def _normal(x: np.ndarray):
-        ############# YOUR CODE #############
-        pass
+        return 1/(1 + np.exp(-x))
 
     @staticmethod
     def _derivate(x: np.ndarray):
-        ############# YOUR CODE #############
-        pass
+        sigmoid = 1/(1+np.exp(-x))
+        return sigmoid*(1-sigmoid)
 
-
+# it looks good
 class ReLU(Activation):
     @staticmethod
     def _normal(x: np.ndarray):
-        ############# YOUR CODE #############
-        pass
+        x[x <= 0] =0
+        return x
 
     @staticmethod
     def _derivate(x: np.ndarray):
-        ############# YOUR CODE #############
-        pass
-
+        x[x > 0] = 1
+        x[x <= 0] = 0
+        return 0
 
 class LeakyReLU(Activation):
     @staticmethod
     def _normal(x: np.ndarray):
-        ############# YOUR CODE #############
-        pass
+        x[x <= 0] = x[x <= 0] *0.1
+        return x
 
     @staticmethod
     def _derivate(x: np.ndarray):
-        ############# YOUR CODE #############
-        pass
+        x[x > 0] = 1
+        x[x <= 0] = 0.1
+        return x
